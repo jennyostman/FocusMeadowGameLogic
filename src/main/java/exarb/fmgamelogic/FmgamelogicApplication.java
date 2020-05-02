c5b84d14b2ac25c5524caa2c99df25bd34a87ce0
Lovisa <lovisa.lybeck@gmail.com>
2020-05-02 14:44:05 +0200

    Eureka setup

--- src/main/java/exarb/fmgamelogic/FmgamelogicApplication.java
+++ src/main/java/exarb/fmgamelogic/FmgamelogicApplication.java
@@ -1,11 +1,10 @@
 package exarb.fmgamelogic;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class FmgamelogicApplication {
 
